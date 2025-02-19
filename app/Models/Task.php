a4ce9e78d2e3ff965781c17d756469f9935929c2
unknown <ebaglgostwenty4@gmail.com>
2025-02-19 17:04:28 +0800

    Finished working Migration and Model.

---
+++ app/Models/Task.php
@@ -0,0 +1,11 @@
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'description', 'is_completed'];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\GreetController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+// Home Route (Redirect to /greet or view welcome page)
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/greet'); // Redirect to /greet
 });
+
+// Greet Route
+Route::get('/greet', [GreetController::class, 'index']);
+
+// Task API Resource Route
+Route::apiResource('tasks', TaskController::class);
